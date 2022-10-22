ba35d1f89f08f4342542b842c4906b7900145914
Alex Maior <alexmaior@yahoo.com>
2022-10-22 23:38:50 +0300

    Added program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        int counter = 1;
+        int sum = 0;
+
+        do{
+            System.out.println("Enter number " + counter + ": ");
+
+            boolean isInt = scanner.hasNextInt();
+
+            if (!isInt){
+                System.out.println("Invalid number!");
+            }else{
+                int num = scanner.nextInt();
+                sum = sum + num;
+                counter++;
+            }
+            scanner.nextLine();
+
+        }while(counter<=10);
+        System.out.println("The sum is : " + sum);
+        scanner.close();
     }
+
+
+
+
 }
